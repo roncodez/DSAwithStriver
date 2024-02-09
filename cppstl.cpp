75adad3b0d914bb4e933695009d61d3bd5f8fdf0
roncodez <ronitmalhotraofficial@gmail.com>
2024-02-09 21:34:17 +0530

    third commmit

--- cppstl.cpp
+++ cppstl.cpp
@@ -169,16 +169,179 @@ void explainPQ() {
     pq.push(2); // {2,5}
     pq.push(8); // {2,5,8}
     pq.emplace(10); // {2,5,8,10}
-
+    //internally a tree data structure is maintained 
     cout << pq.top(); // 10
     pq.pop(); // {2,5,8}	    
 
     cout << pq.top(); // 8
+    // the above is known as a Max-heap 
 
     //size swap empty function same as others 
 
-    // Minimum heap 
+    // Minimum heap - the smallest element at the top 
+    priority_queue<int, vector<int>, greater<int>> pq1; // this is the syntax for a min heap
+    pq1.push(5);
+    pq1.push(2);
+    pq1.push(8);
+    pq1.emplace(10);
+
+    cout << pq.top(); // 2
+
+    /*
+    push/ pop - O(log n)
+    top - O(1)    
+    */
+}
+
+// Set - sorted order and unique elements - tree is maintained  
+void explainSet() {
+    // all operations in log n complexity 
+    set<int> st;
+    st.insert(1); // {1}
+    st.emplace(2); // {1,2}
+    st.insert(2); // {1,2}
+    st.insert(4); // {1,2,4}
+    st.insert(3); // {1,2,3,4}
+
+    // Functionality of insert in vector can be used, that only increases efficiency
+
+    //begin(), end(), rbegin(), rend(), clear(), insert(), size(), swap() are same as those above 
+
+    auto it = st.find(3); // returns an iterator to the element if it exists, else returns end() - points to the memory address 
+
+    auto it = st.find(6);
+
+    st.erase(5); // {1,2,3,4} - erases 5 - takes logarithmic time 
+
+    int cnt = st.count(1);
+
+    auto it = st.find(3);
+    st.erase(it); 
+    /*
+    erase can work in 2 ways -> 1. take in the element to be deleted 2. take in the iterator/ memory location to the element to be deleted
+    */
+    auto it1 = st.find(2);
+    auto it2 = st.find(4);
+    st.erase(it1, it2);
+
+    // lower_bound() and upper_bound() function works in the same way as in vector it does 
+
+    auto it = st.lower_bound(2);
+    auto it = st.upper_bound(3);
+}
+
+void explainMultiSet() {
+    // Everything is same as set, only difference is that duplicate elements are allowed, only sorted not unique 
+    multiset<int> ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(1); // {1,1,1}
+
+    ms.erase(1); // all occurences of 1 erased 
+
+    int cnt = ms.count(1);
+
+    // only a single 1 is erased 
+    ms.erase(ms.find(1));
+
+    ms.erase(ms.find(1), ms.find(1)+2);
+    // rest all functions same as set 
+}
+
+// Unordered set - Unique characters, NOT Sorted - all operations in O(1)
+// worst case - O(n) linear time 
+void explainUSet() {
+    unordered_set<int> st;
+    /*
+    lower_bound and upper_bound function does not work, rest all functions are same as above, it does not 
+    store in any particular order, it has better complexity than set in most cases, 
+    except when collision happens
+    */
+}
+
+// Map 
+// key value pair 
+// keys unique, values can be duplicated 
+void explainMap() {
+    map<int, int> mpp;
+    map<int, pair<int, int>> mpp;
+    map<pair<int, int>, int> mpp;
+
+    mpp[1] = 2;
+    mpp.emplace({3,1}); 
+
+    mpp.insert({2, 4});
+
+    mpp[{2,3}] = 10;
+
+    for(auto it: mpp) {
+        cout << it.first << " " << it.second << endl;
+    }
+
+    cout << mpp[1];
+    cout << mpp[5];
+
+    auto it = mpp.find(3);
+    cout << *(it).second;
     
+    auto it = mpp.find(5);
+    // sorted order of KEY not Values 
+
+    auto it = mpp.lower_bound(2);
+    auto it = mpp.upper_bound(3);
+
+    // erase, swap, size, empty are same as above 
+}
+
+void explainMultiMap() {
+    // everything same as map, only it can store multiple keys 
+    // only mpp[key] cannot be used here
+}
+
+void explainUnorderedMap() {
+    // same as set and unordered set ka difference 
+}
+
+// ALL CONTAINERS DONE 
+
+// Algorithms 
+bool comp(pair<int, int>p1, pair<int, int> p2) {
+    if(p1.second < p2.second) return true;
+    if(p1.second > p2.second) return false;
+    // else they are same 
+    if(p1.first>p2.first) return true;
+    return false; 
+}
+// in the comparator based on the condition, internal swapping is taking place.
+
+void explainExtra() {
+    sort(a, a+n); // array iterator pointers 
+    sort(v.begin(), v.end()); // sorting a vector 
+
+    sort(a+2, a+4);
+    sort(a, a+n, greater<int>); // sort in descending order - comparator 
+
+    pair<int, int> a[] = {{1, 2}, {2, 1}, {4, 1}};
+    // sort it according to second element 
+    // if second element is same, 
+    // then sort it according to first element but in descending order 
+
+    sort(a, a+n, comp); // self-written comparator 
+
+    int num = 7;
+    int cnt = __builtin_popcount(num); // to print the number of set bits of a particular number in binary 
+
+    long long num = 1676767676;
+    int cnt = __builtin_popcountll(num);
+
+    string s = "123";
+    sort(s.begin(), s.end());
+
+    do {
+        cout << s << endl;
+    } while(next_permutation(s.begin(), s.end())); // to print all the permutations of a string 
+
+    int maxi = *max_element(a, a+n); // to find the maximum element 
 }
 
 int main() {
